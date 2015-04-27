038918c538b19916cc9b139b8426600802435bdc
vmanoj <vivekmanoj87@gmail.com>
2015-04-27 17:12:20 +0530

    Added functions to Cursor position and pass that to next function

--- game_simulator.js
+++ game_simulator.js
@@ -34,3 +34,53 @@ function stGame(canvasElement, moveCountElement) {
 		newGame();
 	}
 }
+
+function gOnClick(e)
+{
+	/* Gets the cursor position and passes the row based on the cursor position */
+	var cell = getCursorPosition(e);
+	for (var i = 0; i < gNumPieces; i++) 
+	{
+		if ((gPieces[i].row == cell.row) && (gPieces[i].column == cell.column))
+		{
+			clickOnPiece(i);
+			return;
+		}
+	}
+	clickOnEmptyCell(cell);
+}
+
+function getCursorPosition(e) 
+{
+	/* returns Cell with .row and .column properties */
+	var x;
+	var y;
+	if (e.pageX != undefined && e.pageY != undefined) 
+	{
+		x = e.pageX;
+		y = e.pageY;
+	}
+	else 
+	{
+		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	}
+	x -= gCanvasElement.offsetLeft;
+	y -= gCanvasElement.offsetTop;
+	x = Math.min(x, kBoardWidth * kPieceWidth);
+	y = Math.min(y, kBoardHeight * kPieceHeight);
+	var cell = new Cell(Math.floor(y/kPieceHeight), Math.floor(x/kPieceWidth));
+	return cell;
+}
+
+if (typeof resumeGame != "function") 
+{
+	saveGameState = function()
+	{
+		return false;
+	}
+	resumeGame = function() 
+	{
+		return false;
+	}
+}
