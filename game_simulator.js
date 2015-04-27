857cdd6f7c3a10d699be82abde09a0b4421daa7c
vmanoj <vivekmanoj87@gmail.com>
2015-04-27 16:40:20 +0530

    Game simulator JS file added start function

--- game_simulator.js
+++ game_simulator.js
@@ -1,6 +1,5 @@
 
-function stGame(canvasElement, moveCountElement)
-{
+function stGame(canvasElement, moveCountElement) {
 	if (!canvasElement)
 	{
 		canvasElement = document.createElement("canvas");
@@ -12,4 +11,12 @@ function stGame(canvasElement, moveCountElement)
 		moveCountElement = document.createElement("p");
 		document.body.appendChild(moveCountElement);
 	}
+	var gCanvasElement = canvasElement;
+	alert(gCanvasElement);
+	gCanvasElement.width = kPixelWidth;
+	gCanvasElement.height = kPixelHeight;
+	gCanvasElement.addEventListener("click", gOnClick, false);
+	gMoveCountElem = moveCountElement;
+	alert(gMoveCountElem);
+	gDrawingContext = gCanvasElement.getContext("2d");
 }
