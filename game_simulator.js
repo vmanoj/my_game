9de4d4baf2a75a7c5a17899938ae88a7aa11d21a
vmanoj <vivekmanoj87@gmail.com>
2015-04-27 18:20:35 +0530

    Gets the cursor postion and pass that value.

--- game_simulator.js
+++ game_simulator.js
@@ -2,6 +2,9 @@ var gCanvasElement;
 var gDrawingContext;
 var gMoveCountElem;
 
+var gPieces;
+var gNumPieces;
+
 function stGame(canvasElement, moveCountElement) {
 	if (!canvasElement)
 	{
@@ -30,4 +33,42 @@ function Cell(row, column)
 {
 	this.row = row;
 	this.column = column;
+}
+
+function gOnClick(e)
+{
+	/* Gets the cursor position and passes the row based on the cursor position */
+	var cell = getCursorPosition(e);
+	for (var i = 0; i < gNumPieces; i++) 
+	{
+		if ((gPieces[i].row == cell.row) && (gPieces[i].column == cell.column))
+		{
+			clickOnPiece(i);
+			return;
+		}
+	}
+	clickOnEmptyCell(cell);
+}
+
+function getCursorPosition(e) 
+{
+	/* returns Cell with .row and .column properties */
+	var x;
+	var y;
+	if (e.pageX != undefined && e.pageY != undefined) 
+	{
+		x = e.pageX;
+		y = e.pageY;
+	}
+	else 
+	{
+		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	}
+	x -= gCanvasElement.offsetLeft;
+	y -= gCanvasElement.offsetTop;
+	x = Math.min(x, kBoardWidth * kPieceWidth);
+	y = Math.min(y, kBoardHeight * kPieceHeight);
+	var cell = new Cell(Math.floor(y/kPieceHeight), Math.floor(x/kPieceWidth));
+	return cell;
 }
